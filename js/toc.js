3769e63daca2e9d68e97c93671e5a7080729b025
Ronyx69 <ronyx69@inbox.lv>
2019-02-21 23:13:46 +0200

    e

---
+++ js/toc.js
@@ -0,0 +1,30 @@
+var ToC =
+  "<nav role='navigation' class='table-of-contents'>" +
+    "<ul>";
+var newLine, el, title, link, tagname;
+$(".content h2, .content h3").each(function() {
+  el = $(this);
+  console.log(el[0].tagName);
+  title = el.text();
+  link = "#" + el.attr("id");
+  if(el[0].tagName == "H3") {
+  newLine =
+    "<li><span class='toc2'>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</span></li>";
+  ToC += newLine; }
+  else {
+  newLine =
+    "<li><span class='toc1'>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</span></li>";
+  ToC += newLine; }
+});
+ToC +=
+   "</ul>" +
+  "</nav>";
+$(".toc").append(ToC);
